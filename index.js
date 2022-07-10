f4c4ebbbadab72fe17362b874406bcfd01f6d5ef
SpaceDevelopmentDC <107330373+SpaceDevelopmentDC@users.noreply.github.com>
2022-07-10 11:00:55 +0800

    Added more endpoints

--- index.js
+++ index.js
@@ -26,3 +26,19 @@ module.exports.screenshot = async function(url) {
 	const img = `https://api.spacedc.tk/screenshot?url=${url}`
 	return img;
 }
+module.exports.npm = async function(pkg) {
+  if(!pkg) throw new Error("[SpaceAPI Wrapper] The field 'package name' was left empty in the NPM function!")
+  const url = `https://api.spacedc.tk/npm?q=${encodeURIComponent(pkg)}`
+  const res = await fetch(url)
+  const obj = await res.text()
+  const js = JSON.parse(obj)
+  if(js.error) throw new Error(js.error)
+  else {
+    return js;
+  }
+}
+module.exports.sadcat = async function(text) {
+	if(!text) throw new Error("[SpaceAPI Wrapper] The field 'text' was left empty in the SAD CAT function. Need help? https://popcat.xyz/server")
+	const url = `${baseurl}sadcat?text=${encodeURIComponent(text)}`
+	return url;
+}
